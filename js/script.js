94e6846590a12b80004a986ab2ca4308b1e82136
John Evans <john.evans9103@gmail.com>
2015-02-15 21:05:23 +0000

    Added title screen

--- js/script.js
+++ js/script.js
@@ -8,11 +8,41 @@ $(function() {
 
 
 var TitleScreen = Backbone.View.extend({
+
 	el: '#page',
+
+	events: {
+		'click .begin': 'begin'
+	},
+
 	render: function() {
 		var template = _.template($('#template-title').html());
 		this.$el.html(template);
+
+		var titleText = $('.main-title').html();
+
+		$('.main-title').html('<div class="shadow">' + titleText + '</div><div class="top-text">' + titleText + '</div>');
+
+		$('.main-title .shadow .words').html(function (i, html) {
+			var chars = $.trim(html).split("");
+			return '<span class="real-shadow-text">' + chars.join('</span><span class="real-shadow-text">') + '</span>';
+		});
+
+		$.fn.realshadow.reset();
+
+		$('.page-title .real-shadow-text').realshadow({
+			type: 'text',
+			style: 'flat',
+			// length: 40,
+			color: '5,5,5',
+			// color: '0,0,0'
+		});
+	},
+
+	begin: function() {
+		console.log('begin');
 	}
+
 });
 
 
