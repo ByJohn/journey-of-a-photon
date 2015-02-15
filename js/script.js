0ed87b96d91210856aefcb8dda959bfc79cee738
John Evans <john.evans9103@gmail.com>
2015-02-15 19:12:38 +0000

    Added framework for pages/screens using Backbone.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
 $(function() {
-	
+
 });
+
+
+
+/*------------------- Views/Pages -------------------*/
+
+
+var TitleScreen = Backbone.View.extend({
+	el: '#page',
+	render: function() {
+		var template = _.template($('#template-title').html());
+		this.$el.html(template);
+	}
+});
+
+
+/*------------------- Routes -------------------*/
+
+
+var globalRoutes = [
+	//Name, slug/link/route, view
+	['home', '', new TitleScreen()]
+];
+
+
+/*------------------- Routing -------------------*/
+
+
+var Router = Backbone.Router.extend({
+
+	initialize: function() {
+		for (var i = 0; i < globalRoutes.length; i++) {
+			var route = globalRoutes[i];
+
+			this.route(route[1], route[0], function() {
+				route[2].render();
+			});
+		}
+	},
+
+	routes: {} //The routes are dynamically added using the globalRoutes variable
+
+});
+
+var router = new Router();
+Backbone.history.start();
